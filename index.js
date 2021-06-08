131a4419744f523eb7f4b5c8fcef8211d52088b0
Akarsh Saraff <saraffakarsh@gmail.com>
2021-06-08 18:38:56 +0100

    changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 
 const express = require('express');
 const app = express();
-const port = process.env.port || 3010;
+const port = process.env.PORT || 3000;
 
 const users = ['12345', '54321', '678910']
 
